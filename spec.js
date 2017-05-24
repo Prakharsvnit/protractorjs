3c861fc58656ad1646754f6745354ceea14fdc95
Prakharsvnit <prakhartripathi.4896@gmail.com>
2017-05-24 11:21:24 +0530

    added more actions in spec.js

--- spec.js
+++ spec.js
@@ -5,7 +5,14 @@ browser.waitForAngularEnabled(false);
         browser.get('http://angularjs.org/');    
     });
 
-    it('should click DEVELOP button', function(){   
-       element.all( by.css('li .dropdown-toggle')).get(1).click(); 
+    it('should click DEVELOP and then API Reference button', function(){   
+     var developButton = element.all( by.css('li .dropdown-toggle')).get(1);
+     developButton.click();
+     var api = element(by.xpath('//*[@id="navbar-main"]/div/div/ul/li[2]/ul/li[2]/a'));
+     api.click();
+     var box = element(by.css('.search-query'));
+     box.clear().sendKeys("Filter");
+     var filter = element.all(by.css('.ng-binding')).get(0);
+     filter.click();
     });
 });
